965d55141656d2593052ae48cbbc82c9b08f6141
Jannatim08 <univeruchun@gmail.com>
2024-12-10 21:04:08 +0500

    Add files via upload

---
+++ list_metodss(8-dars).py
@@ -0,0 +1,58 @@
+# 8-dars
+# List methods
+# exercise_1 append()
+# qiymatni ro'yxat oxiriga qo'shadi
+# adjectives=["unique","unusual","interesting"]
+# adjectives.append("unique")
+# print(adjectives)
+
+# exercise_2 clear()
+# ro'yxatdagi barcha elementlarni olib tashlaydi
+# adjectives=["unique" , "unusual" , "intresting" , "beautiful" , "colorful"
+# adjectives.clear()
+# print(adjectives)
+
+# exercise_3 copy()
+# belgilangan ro'yxatdan nusxa olib qaytaradi
+# adjectives=['unique','usualy','beautiful']
+# adjectives.copy()
+# print(adjectives)
+
+# exercise_4 count()
+# elementni ro'yxat oxiriga qo'shadi
+# adjectives=['unique','unusual','interasting', 'beutiful','colorful']
+# x=adjectives.count("boring")
+# print(x)
+
+
+#exercise_5 extend()
+# belgilangan ro'yxat elementlarini (yoki har qanday takrorlanadigan) joriy ro'yxatning oxiriga qo'shadi.
+# adjectives=['unique','unusual','interasting', 'beutiful','colorful']
+# verb=["to work","to write","to do"]
+# x1=.extend(veb)
+# print(adjectives)
+# belgilangan qiymatning birinchi paydo bo'lishidagi pozitsiyani qaytaradi.
+# adjectives=['beautiful','colorful','unusual']
+# x2=adjectives.index(interasting)
+# print(x2)
+
+# exercise_6 pop()
+# belgilangan holatda elementni olib tashlaydi.
+# olib tashlangan qiymatni qaytaradi
+# agar pop() qavsni ichiga qiymat berilmasa oxirgi elementni olib tashlaydi
+# adjectives=['uniqe','intresting','unusual']
+# x3=adjectives.pop(2)
+
+
+# print(x3)# exercise_7 remove()
+# # ko'rsatilgan qiymatga ega elementning birinchi paydo bo'lganini olib tashlaydi.
+# # adjectives=['uniqe','intresting','unusual']
+# # adjectives.remove("olma")
+# # print(adjectives)
+
+
+# exercise_8 remove()
+# ko'rsatilgan qiymatga ega elementning birinchi paydo bo'lganini olib tashlaydi.
+# adjectives=['uniqe','intresting','unusual']
+# adjectives.remove("olma")
+# print(adjectives)
